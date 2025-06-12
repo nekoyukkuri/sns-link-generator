ba8db3d21617f83339c04019916ff597992ffcc1
nekoneko-yukkuri <kuraikoku@gmail.com>
2025-06-12 17:43:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
 document.getElementById("shareForm").addEventListener("submit", function(e) {
   e.preventDefault();
+
   const url = encodeURIComponent(document.getElementById("url").value);
-  const title = encodeURIComponent(document.getElementById("title").value);
+  const text = encodeURIComponent(document.getElementById("text").value);
 
   const buttons = [
     {
       name: "LINE",
       class: "line",
-      link: `https://social-plugins.line.me/lineit/share?url=${url}`
+      icon: "fab fa-line",
+      link: `https://social-plugins.line.me/lineit/share?url=${url}&text=${text}`
     },
     {
       name: "Twitter",
       class: "twitter",
-      link: `https://twitter.com/intent/tweet?url=${url}&text=${title}`
+      icon: "fab fa-twitter",
+      link: `https://twitter.com/intent/tweet?url=${url}&text=${text}`
     },
     {
       name: "Facebook",
       class: "facebook",
+      icon: "fab fa-facebook-f",
       link: `https://www.facebook.com/sharer/sharer.php?u=${url}`
     },
     {
       name: "Threads",
       class: "threads",
-      link: `https://www.threads.net/intent/post?text=${title}%20${url}`
+      icon: "fab fa-threads",
+      link: `https://www.threads.net/intent/post?text=${text}%20${url}`
     },
     {
-      name: "Instagram",
-      class: "instagram",
-      link: `https://www.instagram.com/` // プロフィールリンク推奨
+      name: "Reddit",
+      class: "reddit",
+      icon: "fab fa-reddit",
+      link: `https://www.reddit.com/submit?url=${url}&title=${text}`
     }
   ];
 
-  let html = '';
-  let code = '';
+  let previewHTML = '';
+  let codeHTML = '';
 
   buttons.forEach(btn => {
-    html += `<a href="${btn.link}" class="${btn.class}" target="_blank">${btn.name}</a>\n`;
-    code += `<a href="${btn.link}" class="${btn.class}" target="_blank">${btn.name}</a>\n`;
+    const buttonHTML = `<a href="${btn.link}" class="${btn.class}" target="_blank"><i class="${btn.icon}"></i>${btn.name}</a>`;
+    previewHTML += buttonHTML + "\n";
+    codeHTML += buttonHTML + "\n";
   });
 
-  document.getElementById("preview").innerHTML = html;
-  document.getElementById("code").value = code;
+  document.getElementById("preview").innerHTML = previewHTML;
+  document.getElementById("code").value = codeHTML;
 });
